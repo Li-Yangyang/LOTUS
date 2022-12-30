d8ecf8c5a90ee83ec0bc181317b0af1270b83fff
Li-Yangyang <ly95astro@gmail.com>
2022-12-29 22:00:16 -0500

    update download link for EW library

--- src/lotus_nlte/config.py
+++ src/lotus_nlte/config.py
@@ -38,3 +38,16 @@ if not os.path.exists(EWDIFF_LIB):
     EWDIFF_LIB = GCOG_DIR + "ewdiff_v0.tar.gz"
     url = get_url("ewdiff_v0.tar.gz")
     download_file(url, EWDIFF_DIR)
+
+def fetch_EWLIB_MULTI():
+    if not os.path.exists(os.path.join(fp, "package_data/EWLIB_largergrid2_v0.h5")):
+        import time
+        url = get_url(file_name="EWLIB_largergrid2_v0.h5", record_id="7474663")
+        print("Downloading of the EW library will take around 20 minutes...")
+        t1 = time.time();
+        download_file(url, os.path.join(fp, "package_data/"))
+        t2 = time.time();
+        print("Downloading takes {0:.2f} mins".format((t2-t1) / 60.0))
+    else:
+        print("EW library has already existed: {0:s}".format(os.path.join(fp, "package_data/EWLIB_largergrid2_v0.h5")))
+      
